a2f0b4a3e33054ec8d6b37879d4ccb4407f8685e
A139341238 <elevea139341238@edenschool.fr>
2022-06-30 16:51:43 +0200

    progression of afternoon

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,26 @@ const plats = [ // plats inject json
     {
         imgSrc: "./assets/img/img-01.jpg",
         title:"UN SUPER TITRE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique"
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique dolor dolore error excepturi exercitationem in molestias nam, odit sequi"
     },
     {
         imgSrc: "./assets/img/img-02.jpg",
         title:"UN SUPER TITRE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique"
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique dolor dolore error excepturi exercitatione"
     }, {
         imgSrc: "./assets/img/img-03.jpg",
         title:"UN SUPER TITRE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique?"
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique dolor dolore error excepturi exercitationem in molestias nam, odit sequi?"
     },
     {
         imgSrc: "./assets/img/img-04.jpg",
         title:"UN SUPER TITRE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique?"
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique dolor dolore error excepturi exercitationem in molestias nam, odit sequi"
     },
     {
         imgSrc: "./assets/img/img-05.jpg",
         title:"UN SUPER TITRE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique "
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolor dolore error excepturi exercitationem in molestias nam, odit sequi similique dolor dolore error excepturi exercitationem in molestias nam, odit sequi"
     },
 ];
 let menu = document.getElementById('all-plats'); /* start of dom */
@@ -37,3 +37,14 @@ for (const element of plats){ // I chose as a loop a for of because i think it i
         </div>
     </article>`;
 }
+let dark = document.getElementById("dark"); //declaration of variable
+let body = document.querySelector("body");
+let containerFluid = document.getElementById('container-fluid');
+let h4 = document.createElement('h4');
+function darkMode (){// function to display none the div container and to put the message of th joke
+    body.appendChild(h4);
+    h4.innerHTML= "C'EST UNE BLAGUE ... RECHARGEZ LA PAGE";
+    body.classList.toggle("joke");
+    containerFluid.classList.toggle('none');
+}
+dark.addEventListener("click", darkMode);// event to put the function darkMode
